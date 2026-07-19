a5cdd35b2a41922717167422a655a439a9f82254
Khilesh Bhangale <161333551+Khileshhhub@users.noreply.github.com>
2026-07-19 21:45:55 +0530

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-
+from typing import Optional
+from fastapi import *
 
 app = FastAPI() 
 
+
 tasks = [  
-     { "id" : 1, "title" : "GET", "completed" : False}, 
-     { "id" : 2, "title" : "POST", "completed" : False} , 
-     { "id" : 3, "title" : "PUT", "completed" : False} , 
-     { "id" : 4, "title" : "DELETE", "completed" : False}  ]
+     { "id" : 1, "title" : "Buy gold", "completed" : False}, 
+     { "id" : 2, "title" : "Finish homework", "completed" : False} , 
+     { "id" : 3, "title" : "Do laundry", "completed" : False}  ]
+
 class TaskCreate(BaseModel):
     title: str
+class TaskUpdate(BaseModel):
+    title: Optional[str] = None
+    completed: Optional[bool] = None
+
 
 @app.get("/")
 async def home():
@@ -51,3 +57,59 @@ async def create_task(task: TaskCreate):
     }
     tasks.append(new_task)
     return new_task
+
+
+
+@app.put("/tasks/{id}")
+def update_task(id: int, updated_task: TaskUpdate):
+
+    for task in tasks:
+
+        if task["id"] == id:
+
+            if updated_task.title is not None:
+
+                if updated_task.title.strip() == "":
+                    return JSONResponse(
+                        status_code=400,
+                        content={"error": "Title cannot be empty"}
+                    )
+
+                task["title"] = updated_task.title.strip()
+
+            if updated_task.done is not None:
+                task["done"] = updated_task.done
+
+            return task
+
+    return JSONResponse(
+        status_code=404,
+        content={"error": f"Task {id} not found"}
+    )
+
+
+@app.delete("/tasks/{id}", status_code=204)
+async def delete_task(id: int):
+    for index, task in enumerate(tasks):
+        if task["id"] == id:
+            tasks.pop(index)
+            return Response(status_code=204)
+
+    return JSONResponse(
+        status_code=404,
+        content={"error": f"Task {id} not found"}
+    )
+
+    
+
+
+
+                
+    
+            
+            
+
+
+
+                
+    
